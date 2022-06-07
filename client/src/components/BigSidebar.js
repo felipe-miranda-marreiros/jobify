2226c452e79066b833f0cefa4038ebcbf1a60e33
felipe-miranda-marreiros <felipemirandahoje@gmail.com>
2022-06-07 10:58:16 -0300

    BigSidebar e responsividade

--- client/src/components/BigSidebar.js
+++ client/src/components/BigSidebar.js
@@ -1,9 +1,26 @@
 import Wrapper from '../assets/wrappers/BigSidebar';
 
+import { useAppContext } from '../context/appContext';
+import { NavLinks } from './NavLinks';
+import Logo from './Logo';
+
 export const BigSidebar = () => {
+  const { showSidebar, toggleSidebar } = useAppContext();
+
   return (
     <Wrapper>
-      <h4>Big Sidebar</h4>
+      <div
+        className={
+          showSidebar ? 'sidebar-container show-sidebar' : 'sidebar-container'
+        }
+      >
+        <div className="content">
+          <header>
+            <Logo />
+          </header>
+          <NavLinks toggleSidebar={toggleSidebar} />
+        </div>
+      </div>
     </Wrapper>
   );
 };
