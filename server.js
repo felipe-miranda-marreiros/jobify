fd120d7f01baf2afe833b3a5ccb949b263c4e253
felipe-miranda-marreiros <felipemirandahoje@gmail.com>
2022-06-09 23:56:43 -0300

    Auth, user login, user update

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ import morgan from 'morgan';
 
 import notFoundMiddleware from './middleware/not-found.js';
 import errorHandlerMiddleware from './middleware/error-handler.js';
+import authenticateUser from './middleware/auth.js';
 
 import authRouter from './routes/authRoutes.js';
 import jobsRouter from './routes/jobsRoutes.js';
@@ -22,7 +23,7 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 app.use('/api/v1/auth', authRouter);
-app.use('/api/v1/jobs', jobsRouter);
+app.use('/api/v1/jobs', authenticateUser, jobsRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
