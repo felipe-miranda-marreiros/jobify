3b70a5e8131971ba2dcd8a12eabd29ce5dc62940
felipe-miranda-marreiros <felipemirandahoje@gmail.com>
2022-06-13 22:12:10 -0300

    Atualizar usuario - back e front

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -9,7 +9,10 @@ import {
   SETUP_USER_SUCCESS,
   SETUP_USER_ERROR,
   TOGGLE_SIDEBAR,
-  LOGOUT_USER
+  LOGOUT_USER,
+  UPDATE_USER_BEGIN,
+  UPDATE_USER_SUCCESS,
+  UPDATE_USER_ERROR
 } from './actions';
 
 const token = localStorage.getItem('token');
@@ -33,6 +36,33 @@ const AppContext = React.createContext();
 // Children sera o <App />
 const AppProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState);
+
+  const authFetch = axios.create({
+    baseURL: '/api/v1'
+  });
+
+  //Request
+  authFetch.interceptors.request.use(
+    (config) => {
+      config.headers.common['Authorization'] = `Bearer ${state.token}`;
+      return config;
+    },
+    (error) => {
+      return Promise.reject(error);
+    }
+  );
+
+  //Response
+  authFetch.interceptors.response.use(
+    (response) => {
+      return response;
+    },
+    (error) => {
+      console.log(error.response);
+      if (error.response.status === 401) console.log('AUTH ERROR');
+      return Promise.reject(error);
+    }
+  );
 
   const displayAlert = () => {
     dispatch({ type: DISPLAY_ALERT });
@@ -89,9 +119,39 @@ const AppProvider = ({ children }) => {
     dispatch({ type: LOGOUT_USER });
     removeUserFromLocalStorage();
   };
+
+  const updateUser = async (currentUser) => {
+    dispatch({ type: UPDATE_USER_BEGIN });
+
+    try {
+      const { data } = await authFetch.patch('auth/updateUser', currentUser);
+
+      const { user, location, token } = data;
+
+      dispatch({
+        type: UPDATE_USER_SUCCESS,
+        payload: { user, location, token }
+      });
+
+      addUserToLocalStorage({ user, location, token });
+    } catch (error) {
+      dispatch({
+        type: UPDATE_USER_ERROR,
+        payload: { msg: error.response.data.msg }
+      });
+    }
+    clearAlert();
+  };
   return (
     <AppContext.Provider
-      value={{ ...state, displayAlert, setupUser, toggleSidebar, logoutUser }}
+      value={{
+        ...state,
+        displayAlert,
+        setupUser,
+        toggleSidebar,
+        logoutUser,
+        updateUser
+      }}
     >
       {children}
     </AppContext.Provider>
--- client/src/pages/dashborad/Profile.js
+++ client/src/pages/dashborad/Profile.js
@@ -1,3 +1,57 @@
+import { useState } from 'react';
+import { Alert, FormRow } from '../../components';
+import { useAppContext } from '../../context/appContext';
+import Wrapper from '../../assets/wrappers/DashboardFormPage';
+
 export const Profile = () => {
-  return <div>Profile</div>;
+  const { user, showAlert, updateUser, isLoading } = useAppContext();
+
+  const [name, setName] = useState(user?.name);
+  const [email, setEmail] = useState(user?.email);
+  const [lastName, setLastName] = useState(user?.lastName);
+  const [location, setLocation] = useState(user?.location);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    updateUser({ name, email, lastName, location });
+  };
+
+  return (
+    <Wrapper>
+      <form className="form" onSubmit={handleSubmit}>
+        <h3>profile</h3>
+        {showAlert && <Alert />}
+        <div className="form-center">
+          <FormRow
+            type="text"
+            name="name"
+            value={name}
+            handlerChange={(e) => setName(e.target.value)}
+          />
+          <FormRow
+            type="text"
+            labelText="last name"
+            name="lastName"
+            value={lastName}
+            handlerChange={(e) => setLastName(e.target.value)}
+          />
+          <FormRow
+            type="email"
+            name="email"
+            value={email}
+            handlerChange={(e) => setEmail(e.target.value)}
+          />
+          <FormRow
+            type="text"
+            name="location"
+            value={location}
+            handlerChange={(e) => setLocation(e.target.value)}
+          />
+          <button className="btn btn-block" type="submit" disabled={isLoading}>
+            {isLoading ? 'Please wait...' : 'Save changes'}
+          </button>
+        </div>
+      </form>
+    </Wrapper>
+  );
 };
