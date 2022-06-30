3358bf8ec26b9eb1956de3769f003b777366566d
felipe-miranda-marreiros <felipemirandahoje@gmail.com>
2022-06-29 23:21:52 -0300

    areaChart, bardChart com Recharts

--- client/src/components/ChartsContainer.js
+++ client/src/components/ChartsContainer.js
@@ -1,8 +1,22 @@
+import React, { useState } from 'react';
+
+import AreaChart from './AreaChart';
+import { useAppContext } from '../context/appContext';
+import Wrapper from '../assets/wrappers/ChartsContainer';
+import BarChartComponent from './BarChart';
+
 const ChartsContainer = () => {
+  const [barChart, setBarChart] = useState(true);
+  const { monthlyApplications: data } = useAppContext();
+
   return (
-    <div>
-      <h1>ChartsContainer</h1>
-    </div>
+    <Wrapper>
+      <h4>Montly Applications</h4>
+      <button type="button" onClick={() => setBarChart(!barChart)}>
+        {barChart ? 'Area Chart' : 'Bar Chart'}
+      </button>
+      {barChart ? <BarChartComponent data={data} /> : <AreaChart data={data} />}
+    </Wrapper>
   );
 };
 
